c63a75484b8558d4390c0468a126ad1ff368ec08
Chris Groves <chris_groves87@hotmail.co.uk>
2019-09-26 16:31:54 +0100

    refactor: extract repeating test setup to a method

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require './app'
 
 feature 'Player 2 Hit Points' do
   scenario "Player 1 can see Player 2's Hit Points" do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Chris'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John vs. Chris - 100 HP'
   end
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -2,10 +2,7 @@ require './app'
 
 feature 'Player name submission and display' do
   scenario 'Players can submit names via a form and see them on the screen' do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Chris'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John vs. Chris'
   end
 end
