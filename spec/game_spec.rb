ed37b496ffa72565f6d7d96cee9a368efd2becf0
Chris Groves <chris_groves87@hotmail.co.uk>
2019-09-27 12:33:17 +0100

    refactor: use game class to access players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,25 @@
 require './lib/game'
 
 describe Game do
+  it 'accepts the first player' do
+    player_1 = Player.new('John')
+    player_2 = Player.new('Chris')
+    game = Game.new(player_1, player_2)
+    expect(game.player_1).to eq player_1
+  end
+
+  it 'accepts the second player' do
+    player_1 = Player.new('John')
+    player_2 = Player.new('Chris')
+    game = Game.new(player_1, player_2)
+    expect(game.player_2).to eq player_2
+  end
+
   describe '#attack' do
     it 'reduces hit points' do
       john = Player.new('John')
       chris = Player.new('Chris')
-      game = Game.new
+      game = Game.new(john, chris)
       expect { game.attack(chris) }.to change { chris.hit_points }.by(-10)
     end
   end
